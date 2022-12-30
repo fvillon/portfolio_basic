d5a258a44c3249fa31ee4d28abbc2a33fa9c414e
fvillon <fvillonr@gmail.com>
2022-12-30 15:49:56 -0500

    responsive menu part1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let menu_icon = document.querySelector("#menu_icon");
+let menu = document.querySelector(".menu");
+
+menu_icon.onclick = () => {
+  menu.classList.toogle("open");
+};
+
+alert("Hola mundo");
