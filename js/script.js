d0f739f426ff050ae918fbe27d1120e817803448
fvillon <fvillonr@gmail.com>
2022-12-31 16:35:13 -0500

    scroll effect finished

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let iconMenu = document.getElementById("iconMenu");
 let menu = document.querySelector(".menu");
 let menu_icon = document.querySelector("#menu_icon");
 const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll("nav .menu ul li");
+const navLi = document.querySelectorAll("nav #actionsNav ul li a");
 
 menu_icon.onclick = () => {
   menu.classList.toggle("open");
@@ -23,4 +23,19 @@ document.addEventListener("scroll", () => {
   }
 });
 
-window.addEventListener("scroll", () => {});
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
